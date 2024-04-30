6feca9903e0b8c468968b7ef52f5c7a160eaa598
Quinn Plumb <quinnplumb@Quinns-MacBook-Pro-3.local>
2024-04-30 14:43:30 -0400

    added files

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ListOfMovies from './ListOfMovies';
-import MovieDetails from './MovieDetails';
 import Blackjack from './Blackjack';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
